d33750380aded41d7255f25c562c9b9726c398ad
Leandrho <leandro.coder01@gmail.com>
2025-07-15 15:28:54 -0300

    add UserRole value obj

---
+++ src/lib/user/domain/value-objects/UserRole.ts
@@ -0,0 +1,21 @@
+import { StringValueObject } from "src/lib/shared/domain/value-objects/StringValueObject";
+import { ValidationResult } from "src/lib/shared/domain/value-objects/types";
+
+export class UserRole extends StringValueObject{
+    
+    private static readonly validRoles: string[] = ['admin', 'user', 'guest'];
+
+    constructor(role: string){
+        super(role);
+    }
+
+    public isValid(value: string): ValidationResult {
+        
+        if(!UserRole.validRoles.includes(value))
+            return {isValid: false, error: {errorMsg: 'Domain error: Invalid user role', propName: 'UserRole', value: value}};
+
+        return {isValid: true};
+    }
+
+
+}

082bd4cd47461d570bc671aa682562fc39a5994e
Leandrho <leandro.coder01@gmail.com>
2025-07-16 17:23:25 -0300

    add UserRespository implementation InMemory

---
+++ src/lib/user/infrastructure/persistence/InMemoryUserRepository.ts
@@ -0,0 +1,34 @@
+import { User } from "../../domain/entities/User";
+import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { UserId, UserEmail } from "../../domain/value-objects";
+
+export class InMemoryUserRepository implements IUserRepository{
+
+    private userStorage: Map<string, User> = new Map<string, User>();
+    
+    public async save(user: User): Promise<void> {
+        this.userStorage.set(user.id.value(), user);
+    }
+
+    public async delete(id: UserId): Promise<void> {
+        this.userStorage.delete(id.value());
+    }
+
+    public async findById(id: UserId): Promise<User | null> {
+        return this.userStorage.get(id.value()) ?? null;
+    }
+
+    public async findByEmail(email: UserEmail): Promise<User | null> {
+        for (const [ , value ] of this.userStorage) {
+            if (value.email.value() === email.value()) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public async findAll(): Promise<User[]> {
+        return Array.from(this.userStorage.values());
+    }
+    
+}

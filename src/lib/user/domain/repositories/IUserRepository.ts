0c8c32e212ca619e27009fe4c2e1fceb078b9055
Leandrho <leandro.coder01@gmail.com>
2025-07-16 17:22:52 -0300

    ...

--- src/lib/user/domain/repositories/IUserRepository.ts
+++ src/lib/user/domain/repositories/IUserRepository.ts
@@ -1,12 +1,12 @@
 import { User } from "../entities/User";
 import { UserEmail, UserId } from "../value-objects";
 
-export interface IUserRespository {
+export interface IUserRepository {
 
     save(user: User): Promise<void>;
     delete(id: UserId): Promise<void>;
-    findById(id: UserId): Promise<User | undefined>;
-    findByEmail(email: UserEmail): Promise<User | undefined>;
+    findById(id: UserId): Promise<User | null>;
+    findByEmail(email: UserEmail): Promise<User | null>;
     findAll(): Promise<User[]>;
-    
+
 }

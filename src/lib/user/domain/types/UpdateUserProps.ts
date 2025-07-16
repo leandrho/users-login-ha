cf6741815bebe0a9c83a4acfbfc2489afd62078b
Leandrho <leandro.coder01@gmail.com>
2025-07-16 16:34:10 -0300

    puede servir en el futuro

---
+++ src/lib/user/domain/types/UpdateUserProps.ts
@@ -0,0 +1,4 @@
+
+//! Con esto puedo hacer una actualizacion de un User apartir de tipos primitivos, que pueden venir de un bd, etc, en User deberia tener un metodo updateFromPrimitives(props: UpdateUserProps).. 
+//! Despues revisar si vale la pena..
+export type UpdateUserProps = Partial<{ fullName: string, password: string, role: string, status: string }>;

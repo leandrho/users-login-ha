e8fb7c759031cc89f8022030c389554e80ba43d4
Leandrho <leandro.coder01@gmail.com>
2025-07-17 17:14:05 -0300

    update usecases

--- src/lib/user/application/use-cases/UserCreateUseCase.ts
+++ src/lib/user/application/use-cases/UserCreateUseCase.ts
@@ -1,12 +1,35 @@
 import { User } from "../../domain/entities/User";
+import { UserDuplicatedEmailError } from "../../domain/errors/UserEmailDuplicatedError";
+import { UserInvalidPropertyError } from "../../domain/errors/UserInvalidPropertyError";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { UserEmail } from "../../domain/value-objects";
+import { UserCreatedOutDTO } from "../dtos/UserCreatedOutDTO";
+import { UserCreateInDTO } from "../dtos/UserCreateInDTO";
 
 export class UserCreateUseCase {
     
     constructor(private readonly userRepository: IUserRepository){}
 
-    public async execute( fullname: string, email: string, password: string, role: string): Promise<void>{
-        const newUser: User = User.createNew(fullname, email, password, role);
+    public async execute( createUser: UserCreateInDTO ): Promise<UserCreatedOutDTO>{
+
+        const user: User | null = await this.userRepository.findByEmail(new UserEmail(createUser.email));
+        if(user)
+            throw new UserDuplicatedEmailError(createUser.email);
+        
+        const newUser: User = User.createNew(createUser.fullname, createUser.email, createUser.password, createUser.role);
         await this.userRepository.save(newUser);
+
+        const userPrimitive = newUser.toPrimitives();
+
+        return { 
+            id: userPrimitive.id, 
+            fullName: userPrimitive.fullName, 
+            email: userPrimitive.email, 
+            role: userPrimitive.role, 
+            createdAt: userPrimitive.createdAt, 
+            status: userPrimitive.status, 
+            lastLogin: userPrimitive.lastLogin, 
+        };
+
     }
 }
--- src/lib/user/application/use-cases/UserUpdatePasswordUseCase.ts
+++ src/lib/user/application/use-cases/UserUpdatePasswordUseCase.ts
@@ -2,16 +2,21 @@ import { User } from '../../domain/entities/User';
 import { UserNotFoundError } from '../../domain/errors/UserNotFoundError';
 import { IUserRepository } from '../../domain/repositories/IUserRepository'
 import { UserId, UserPassword } from '../../domain/value-objects';
+import { UserInvalidOldPasswordError } from '../../domain/errors/UserInvalidOldPasswordError';
+import { UserUpdatePasswordInDTO } from '../dtos/UserUpdatePasswordInDTO';
 
 export class UserUpdatePasswordUseCase{
     constructor(private readonly userRepository: IUserRepository ){}
     
-    public async execute(id: string, password: string): Promise<void>{
-            const user: User | null = await this.userRepository.findById(new UserId(id));
+    public async execute(data: UserUpdatePasswordInDTO): Promise<void>{
+            const user: User | null = await this.userRepository.findById(new UserId(data.userId));
             if(!user){
                 throw new UserNotFoundError('User not found - cannot update password.');
             }
-            user.updatePassword(new UserPassword(password))
+            if(user.password.value() !== data.oldPassword)
+                throw new UserInvalidOldPasswordError();
+
+            user.updatePassword(new UserPassword(data.newPassword));
             await this.userRepository.save(user);
     }
 }

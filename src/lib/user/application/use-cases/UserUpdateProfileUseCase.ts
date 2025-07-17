9ece26b0a5db0f4b7fb905a329e3da2837c5488c
Leandrho <leandro.coder01@gmail.com>
2025-07-17 17:14:32 -0300

    add update profile usecase

---
+++ src/lib/user/application/use-cases/UserUpdateProfileUseCase.ts
@@ -0,0 +1,38 @@
+import { User } from "../../domain/entities/User";
+import { UserNotFoundError } from "../../domain/errors/UserNotFoundError";
+import { IUserRepository } from "../../domain/repositories/IUserRepository";
+import { UpdateUserProps } from "../../domain/types/UpdateUserProps";
+import { UserFullname, UserId, UserRole, UserStatus } from "../../domain/value-objects";
+import { UserUpdatedProfileOutDTO } from "../dtos/UserUpdatedProfileOutDTO";
+
+export class UserUpdateProfileUseCase{
+    
+    constructor(private readonly userRepository: IUserRepository){}
+
+    public async execute( id: string, props: UpdateUserProps ): Promise<UserUpdatedProfileOutDTO>{
+        const user: User | null = await this.userRepository.findById(new UserId(id));
+        if(!user)
+            throw new UserNotFoundError('User not found - cannot update profile.');
+
+        if(props.fullName)
+            user.updateFullname(new UserFullname(props.fullName));
+        if(props.role)
+            user.updateRole(new UserRole(props.role));
+        if(props.status)
+            user.updateStatus(new UserStatus(props.status))
+
+        await this.userRepository.save(user);
+        const userPrimitive = user.toPrimitives();
+
+        return { 
+            id: userPrimitive.id, 
+            fullName: userPrimitive.fullName, 
+            email: userPrimitive.email, 
+            role: userPrimitive.role, 
+            createdAt: userPrimitive.createdAt, 
+            status: userPrimitive.status, 
+            lastLogin: userPrimitive.lastLogin, 
+        };
+    }
+
+}

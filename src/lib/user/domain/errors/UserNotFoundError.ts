63ffc3a7cde4c72b9d6faa738f957bab78f604e6
Leandrho <leandro.coder01@gmail.com>
2025-07-17 17:14:45 -0300

    ..

--- src/lib/user/domain/errors/UserNotFoundError.ts
+++ src/lib/user/domain/errors/UserNotFoundError.ts
@@ -1,5 +1,6 @@
+import { DomainRuleViolationError } from "./DomainRuleViolationError";
 
-export class UserNotFoundError extends Error{
+export class UserNotFoundError extends DomainRuleViolationError{
     constructor(message: string = 'User not found.'){
         super(message);
         this.name = 'UserNotFoundError';

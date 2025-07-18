8833af7e2d6ec3bfb551cc519d856dd498e5c420
Leandrho <leandro.coder01@gmail.com>
2025-07-18 17:13:49 -0300

    algunos zod schemas

---
+++ src/lib/user/infrastructure/http/user-schemas.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+export const createUserSchema = z.object({
+    fullname: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(8),
+    role: z.enum(['admin', 'user', 'guest']),
+});
+
+export const updateProfileSchema = z.object({
+    fullname: z.string().min(3).optional(),
+    password: z.string().min(8).optional(),
+    role: z.enum(['admin', 'user', 'guest']).optional(),
+    status: z.enum(['active', 'inactive', 'banned']).optional(),
+});
+
+export const userIdSchema = z.uuidv4('Invalid user id format');
+
+export const userUpdatePasswordSchema = z.object({
+    oldPassword: z.string().min(8),
+    newPassword: z.string().min(8),
+});

25e0c88a76c9b102c4849038ed18447000411a1c
Leandrho <leandro.coder01@gmail.com>
2025-07-18 15:17:02 -0300

    update updatepass usecase

--- src/lib/user/application/use-cases/UserUpdatePasswordUseCase.ts
+++ src/lib/user/application/use-cases/UserUpdatePasswordUseCase.ts
@@ -4,19 +4,28 @@ import { IUserRepository } from '../../domain/repositories/IUserRepository'
 import { UserId, UserPassword } from '../../domain/value-objects';
 import { UserInvalidOldPasswordError } from '../../domain/errors/UserInvalidOldPasswordError';
 import { UserUpdatePasswordInDTO } from '../dtos/UserUpdatePasswordInDTO';
+import { UserInvalidPasswordError } from '../../domain/errors/UserInvalidPasswordError';
+import { IPasswordHasher } from 'src/lib/shared/application/security/IPasswordHasher';
 
 export class UserUpdatePasswordUseCase{
-    constructor(private readonly userRepository: IUserRepository ){}
+    constructor(private readonly userRepository: IUserRepository, private readonly passwordHasher: IPasswordHasher ){}
     
     public async execute(data: UserUpdatePasswordInDTO): Promise<void>{
             const user: User | null = await this.userRepository.findById(new UserId(data.userId));
             if(!user){
                 throw new UserNotFoundError('User not found - cannot update password.');
             }
-            if(user.password.value() !== data.oldPassword)
+            
+            if(!data.newPassword || !data.oldPassword)
+                throw new UserInvalidPasswordError('Password is invalid.');
+            
+            const matchOldPass: boolean = await this.passwordHasher.compare(data.oldPassword, user.password.value());
+            if(!matchOldPass)
                 throw new UserInvalidOldPasswordError();
 
-            user.updatePassword(new UserPassword(data.newPassword));
+            const newPassHash: string = await this.passwordHasher.hash(data.newPassword);
+            user.updatePassword(new UserPassword(newPassHash));
+
             await this.userRepository.save(user);
     }
 }

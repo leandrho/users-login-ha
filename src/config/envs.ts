24a06b2ce6eedac00f5672e482dd4fdaa917f1d4
Leandrho <leandro.coder01@gmail.com>
2025-07-22 15:34:22 -0300

    ups miss envs file

---
+++ src/config/envs.ts
@@ -0,0 +1,17 @@
+
+import env from 'env-var'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const envs = {
+    PORT: env.get('PORT').required().asPortNumber(),
+    AUTH: {
+        JWT_ACCESS_SECRET: env.get('JWT_ACCESS_SECRET').required().asString(),
+        JWT_REFRESH_SECRET: env.get('JWT_REFRESH_SECRET').required().asString(),// no se usa todavia
+        JWT_ACCESS_EXPIRATION_TIME: env.get('JWT_ACCESS_EXPIRATION_TIME').required().asString(),
+        JWT_REFRESH_EXPIRATION_TIME: env.get('JWT_REFRESH_EXPIRATION_TIME').required().asString(),
+    }
+
+}
+export default envs;

f37c338d7d2f7d00abada92c9d3b0725f7f2458c
Leandrho <leandro.coder01@gmail.com>
2025-07-24 12:29:51 -0300

    add register uc to service

--- src/lib/auth/infrastructure/http/AuthService.ts
+++ src/lib/auth/infrastructure/http/AuthService.ts
@@ -1,11 +1,15 @@
-import { AuthUserLoginInDTO, AuthUserLoginOutDTO } from '../../application/dtos';
+import { AuthUserLoginInDTO, AuthUserLoginOutDTO, RegisterUserInDTO, RegisterUserOutDTO } from '../../application/dtos';
+import { AuthRegisterUserUseCase } from '../../application/use-cases/AuthRegisterUserUseCase';
 import { AuthUserLoginUseCase } from '../../application/use-cases/AuthUserLoginUseCase';
 
 export class AuthService {
-    constructor(private readonly authLogin: AuthUserLoginUseCase){}
+    constructor(private readonly authLogin: AuthUserLoginUseCase, private readonly authRegister: AuthRegisterUserUseCase){}
 
     public async login(userCred: AuthUserLoginInDTO): Promise<AuthUserLoginOutDTO>{
         return await this.authLogin.execute(userCred);
     }
 
+    public async register(userRegister: RegisterUserInDTO): Promise<RegisterUserOutDTO>{
+        return await this.authRegister.execute(userRegister);
+    }
 }

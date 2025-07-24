bfeedb0eebaa468d63d2b33434cf960296f1bbe6
Leandrho <leandro.coder01@gmail.com>
2025-07-24 12:30:41 -0300

    add register schema

--- src/lib/auth/infrastructure/http/auth-schemas.ts
+++ src/lib/auth/infrastructure/http/auth-schemas.ts
@@ -5,3 +5,9 @@ export const authLoginSchema = z.object({
     email: z.email('Invalid email format'),
     password: z.string().min(8),
 });
+
+export const registerUserSchema = z.object({
+    fullName: z.string().min(3),
+    email: z.email('Invalid email format'),
+    password: z.string().min(8),
+});

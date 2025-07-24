2d5332248d23c71ca3dbee00bbcf1297c420cefc
Leandrho <leandro.coder01@gmail.com>
2025-07-24 12:30:15 -0300

    add register to controller

--- src/lib/auth/infrastructure/http/AuthController.ts
+++ src/lib/auth/infrastructure/http/AuthController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { AuthService } from "./AuthService";
 import { AuthenticationFailedError } from "../../domain/errors/AuthenticationFailedError";
-import { authLoginSchema } from "./auth-schemas";
+import { authLoginSchema, registerUserSchema } from "./auth-schemas";
+import { RegisterUserInDTO } from '../../application/dtos/RegisterUserInDTO';
+import { UserDuplicatedEmailError, UserInvalidPasswordError, UserInvalidPropertyError } from "../../../user/domain/errors";
 
 export class AuthController{
 
@@ -28,6 +30,33 @@ export class AuthController{
             console.error("Auth Controller Error: ", { error });
         }
 
+    }
+
+    public async register(req: Request, res: Response):Promise<void>{
+        try {
+            
+            const userInfo = registerUserSchema.safeParse(req.body);
+            if(!userInfo.success){
+                res.status(400).json({message: 'Invalid request body data', error: userInfo.error.message});
+                return;
+            }
+            const registerUserDto: RegisterUserInDTO = userInfo.data;
+            const user = await this.authService.register(registerUserDto);
+            res.status(201).json(user);
+
+        } catch (error) {
+             if(error instanceof UserDuplicatedEmailError)
+                res.status(409).json({message: error.message});
+            else if(error instanceof UserInvalidPropertyError)
+                res.status(400).json({message: error.message, property: error.propName});
+            else if(error instanceof UserInvalidPasswordError)
+                res.status(400).json({message: error.message});
+            else {
+                res.status(500).json( {message: 'Internal server error'});
+                console.error("Internal Server Error:", error); // Log the error for debugging
+            }
+        }
+            
     }
 
 }

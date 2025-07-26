bdd5e4266dd65b89cdc73cb5186fa60d958c3335
Leandro Ruiz <leandro.coder01@gmail.com>
2025-07-26 15:20:26 -0300

    add env

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -11,6 +11,7 @@ const envs = {
         JWT_REFRESH_SECRET: env.get('JWT_REFRESH_SECRET').required().asString(),// no se usa todavia
         JWT_ACCESS_EXPIRATION_TIME: env.get('JWT_ACCESS_EXPIRATION_TIME').required().asString(),
         JWT_REFRESH_EXPIRATION_TIME: env.get('JWT_REFRESH_EXPIRATION_TIME').required().asString(),
+        RESET_PASSWORD_URL: env.get('RESET_PASSWORD_URL').required().asString()
     }
 
 }

e9995b8f0f9887658818ff4b5135acc5eae2e589
Leandro Ruiz <leandro.coder01@gmail.com>
2025-07-27 20:24:22 -0300

    append reset pass to service

--- src/lib/auth/infrastructure/http/AuthService.ts
+++ src/lib/auth/infrastructure/http/AuthService.ts
@@ -1,9 +1,14 @@
 import { AuthUserLoginInDTO, AuthUserLoginOutDTO, RegisterUserInDTO, RegisterUserOutDTO } from '../../application/dtos';
 import { AuthRegisterUserUseCase } from '../../application/use-cases/AuthRegisterUserUseCase';
 import { AuthUserLoginUseCase } from '../../application/use-cases/AuthUserLoginUseCase';
+import { RequestPasswordResetUseCase } from '../../application/use-cases/RequestPasswordResetUseCase';
 
 export class AuthService {
-    constructor(private readonly authLogin: AuthUserLoginUseCase, private readonly authRegister: AuthRegisterUserUseCase){}
+    constructor(
+        private readonly authLogin: AuthUserLoginUseCase, 
+        private readonly authRegister: AuthRegisterUserUseCase,
+        private readonly requestPasswordResetUC: RequestPasswordResetUseCase
+    ){}
 
     public async login(userCred: AuthUserLoginInDTO): Promise<AuthUserLoginOutDTO>{
         return await this.authLogin.execute(userCred);
@@ -12,4 +17,9 @@ export class AuthService {
     public async register(userRegister: RegisterUserInDTO): Promise<RegisterUserOutDTO>{
         return await this.authRegister.execute(userRegister);
     }
+
+    public async requestPasswordReset(email: string): Promise<void>{
+        return await this.requestPasswordResetUC.execute({email});
+    }
+
 }

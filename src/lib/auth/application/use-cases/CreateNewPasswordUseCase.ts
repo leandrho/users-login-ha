53036ce03c775e4ec711221a838ee46803644300
Leandrho <leandro.coder01@gmail.com>
2025-07-28 10:02:43 -0300

    update use cases

--- src/lib/auth/application/use-cases/CreateNewPasswordUseCase.ts
+++ src/lib/auth/application/use-cases/CreateNewPasswordUseCase.ts
@@ -1,13 +1,12 @@
 import { IPasswordHasher } from "../../../shared/application/security/IPasswordHasher";
 import { IPasswordResetTokenRepository } from "../../domain/repository/IPasswordResetTokenRepository";
-import { CreateNewPasswordInDTO } from "../dtos/CreateNewPasswordInDTO";
+import { CreateNewPasswordInDTO, CreateNewPasswordOutDTO } from "../dtos";
 import { PasswordResetTokenValue } from "../../domain/value-objects";
 import { IUserRepository } from "../../../user/domain/repositories/IUserRepository";
-import { CreateNewPasswordInvalidTokenError } from "../../domain/errors/CreateNewPasswordInvalidToken";
-import { CreateNewPasswordMismatchError } from "../../domain/errors/CreateNewPasswordMismatchError";
+import { CreateNewPasswordInvalidTokenError, CreateNewPasswordMismatchError, CreateNewPasswordAlreadyUserdTokenError, CreateNewPasswordExpiredTokenError } from "../../domain/errors";
 import { UserNotFoundError } from "../../../user/domain/errors";
 import { UserPassword } from "../../../user/domain/value-objects";
-import { CreateNewPasswordExpiredTokenError } from "../../domain/errors/CreateNewPasswordExpiredToken";
+
 export class CreateNewPasswordUseCase{
     constructor(
         private readonly passwordResetTokenRepository: IPasswordResetTokenRepository,
@@ -15,15 +14,20 @@ export class CreateNewPasswordUseCase{
         private readonly userRepository : IUserRepository
     ){}
 
-    public async execute(createNewPasswordIn: CreateNewPasswordInDTO): Promise<void>{
-        const token = await this.passwordResetTokenRepository.findByToken(new PasswordResetTokenValue(createNewPasswordIn.token));
-        if(!token || token.isUsed())
-            throw new CreateNewPasswordInvalidTokenError('Invalid token');
-        if(token.isExpired())
-            throw new CreateNewPasswordExpiredTokenError('Token expired');
-
+    public async execute(createNewPasswordIn: CreateNewPasswordInDTO): Promise<CreateNewPasswordOutDTO>{
+      
         if(createNewPasswordIn.newPassword !== createNewPasswordIn.confirmPassword)
             throw new CreateNewPasswordMismatchError('Passwords do not match');
+
+        const token = await this.passwordResetTokenRepository.findByToken(new PasswordResetTokenValue(createNewPasswordIn.token));
+        if( !token )
+            throw new CreateNewPasswordInvalidTokenError('Invalid or non-existent password reset token.');
+
+         if( token.isUsed() )
+            throw new CreateNewPasswordAlreadyUserdTokenError('Password reset token has already been used.');
+
+        if(token.isExpired())
+            throw new CreateNewPasswordExpiredTokenError('Token expired');
 
         const user = await this.userRepository.findById(token.userId);
         if(!user)
@@ -35,6 +39,8 @@ export class CreateNewPasswordUseCase{
 
         token.markAsUsed();
         await this.passwordResetTokenRepository.save(token);
+
+        return {message: 'Password reset successfully'}
     
     }
 }
--- src/lib/auth/application/use-cases/RequestPasswordResetUseCase.ts
+++ src/lib/auth/application/use-cases/RequestPasswordResetUseCase.ts
@@ -1,9 +1,8 @@
 import { IUserRepository } from "../../../user/domain/repositories/IUserRepository";
 import { IPasswordResetTokenRepository } from "../../domain/repository/IPasswordResetTokenRepository";
 import { IEmailService } from "../../../shared/application/email/IEmailService";
-import { RequestPasswordResetInDTO } from "../dtos/RequestPasswordResetInDTO";
+import { RequestPasswordResetInDTO, RequestPasswordResetOutDTO } from "../dtos";
 import { UserEmail } from "../../../user/domain/value-objects";
-import { UserNotFoundError } from "../../../user/domain/errors";
 import { PasswordResetToken } from "../../domain/entities/PasswordResetToken";
 import envs from "../../../../config/envs";
 
@@ -16,14 +15,30 @@ export class RequestPasswordResetUseCase{
         private readonly emailService: IEmailService
     ){}
 
-    public async execute(data: RequestPasswordResetInDTO): Promise<void>{
+    public async execute(data: RequestPasswordResetInDTO): Promise<RequestPasswordResetOutDTO>{
         const user = await this.userRespository.findByEmail(new UserEmail(data.email));
+
         if(!user)
-            throw new UserNotFoundError(`Invalid user email ${data.email}`);
+            return {message: 'If an account with that email exists, a password reset link has been sent.'};
+         
+        await this.passwordResetTokenRepository.deleteByUserId(user.id);
+
         const token: PasswordResetToken = PasswordResetToken.createNew(user.id.value());
         await this.passwordResetTokenRepository.save(token);
 
-        await this.emailService.send(data.email, `${envs.AUTH.RESET_PASSWORD_URL}?token=${token.token.value()}`)
+        const subject: string = 'Password reset request for your account.';
+        const body: string = `<p>Hello,</p>
+            <p>You have requested to reset the password for your account.</p>
+            <p>Please click on the following link to reset your password:</p>
+            <p><a href="${envs.AUTH.RESET_PASSWORD_URL}?token=${token.token.value()}">${envs.AUTH.RESET_PASSWORD_URL}?token=${token.token.value()}</a></p>
+            <p>This link will expire in ${envs.AUTH.RESET_PASSWORD_TOKEN_EXPIRATION_MINUTES || 10} minutes.</p>
+            <p>If you did not request a password reset, please ignore this email.</p>
+            <p>Thank you,</p>
+            <p>Your Application Team</p>$
+            `
+
+        await this.emailService.send(data.email, subject, body);
+        return {message: 'If an account with that email exists, a password reset link has been sent.'};
     }
 
 }

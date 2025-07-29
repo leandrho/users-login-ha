a4a0e4a386c71eee56fd508722d81a36ac71fbe2
Leandrho <leandro.coder01@gmail.com>
2025-07-29 09:24:50 -0300

    using reset, request, mailer etc..

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,16 @@ dotenv.config();
 const app: Application = express();
 
 app.use(json());
+
+app.use('/api/users/', authenticateTokenMid(authTokenService), userRouter.router);
+app.use('/api/auth/', authRouter.router)
+
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     console.error("Global Error Handler:", err);
     const status = err.statusCode || 500;
     const message = err.message || 'An unexpected error occurred.';
     res.status(status).json({ message: message });
 });
-
-app.use('/api/users/', authenticateTokenMid(authTokenService), userRouter.router);
-app.use('/api/auth/', authRouter.router)
 
 const port: number = parseInt(process.env.PORT ?? "3000");
 

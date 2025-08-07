44035ce015633d8b7a65377bd94b48a759976c78
Leandrho <leandro.coder01@gmail.com>
2025-08-07 17:40:52 -0300

    removo propname arg

--- src/lib/shared/domain/value-objects/UrlValueObject.ts
+++ src/lib/shared/domain/value-objects/UrlValueObject.ts
@@ -5,18 +5,15 @@ export class UrlValueObject extends StringValueObject{
 
     private static readonly URL_REGEX = /^(?:(?:https?|ftp|sftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
 
-    private readonly _propName: string;
-
-    constructor(url: string, propName: string){
+    constructor(url: string){
         super(url);
-        this._propName = propName;
     }
 
     public isValid(value: string): ValidationResult {
         if(UrlValueObject.URL_REGEX.test(value))
             return {isValid: true}
 
-        return {isValid: false, error: {errorMsg: 'Invalid url', propName: this._propName, value: value}}
+        return {isValid: false, error: {errorMsg: 'Invalid url', propName:'url', value: value}}
     }
 
 }

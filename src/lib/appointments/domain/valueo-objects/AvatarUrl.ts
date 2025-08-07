c25d5b868b9a02c131ef637f9f27874c6eb838b1
Leandrho <leandro.coder01@gmail.com>
2025-08-07 17:41:24 -0300

    update avatar url

--- src/lib/appointments/domain/valueo-objects/AvatarUrl.ts
+++ src/lib/appointments/domain/valueo-objects/AvatarUrl.ts
@@ -1,9 +1,22 @@
+import { ValidationResult } from "src/lib/shared/domain/types";
 import { UrlValueObject } from "src/lib/shared/domain/value-objects/UrlValueObject";
 
 export class AvatarUrl extends UrlValueObject{
 
     constructor(url: string){
-        super(url, 'AvatarUrl');
+        super(url);
     }
-
+    public isValid(value: string): ValidationResult {
+        const resValidation: ValidationResult = super.isValid(value);
+        if(!resValidation.isValid)
+            return {
+                isValid: false,
+                error: {
+                    errorMsg: 'Invalid avatar url',
+                    propName: 'AvatarUrl',
+                    value: value
+                }
+            };
+        return resValidation;
+    }
 }
